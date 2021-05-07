a6f843032f769a02525a7303b958950d77c49ed2
Crow <Crow@DESKTOP-5Q0AVHU>
2021-05-06 19:59:25 -0500

    Hola profesor, estos son los casos de la guia 2 que usted nos dejo de forma grupal

---
+++ src/Solucion/Caso15.java
@@ -0,0 +1,22 @@
+package Solucion;
+
+import java.util.*;
+
+public class Caso15 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese Cliente: ");
+		String cliente = sc.nextLine();
+		System.out.print("Ingrese producto [TB,LT,MN,IM]: ");
+		String producto = sc.nextLine();
+		System.out.print("Ingrese cantidad: ");
+		int cantidad = sc.nextInt();
+		System.out.print("Forma de pago: ");
+		String formaPago = sc.nextLine();
+
+
+	}
+
+}
